cf1ef76e104e2f7c133e6228629ffb833354c083
WuanDuc <univerbachtuoc@gmail.com>
2024-12-23 22:48:12 +0700

    aujust order, add script

--- product_management/product_management/doctype/orders_item/orders_item.js
+++ product_management/product_management/doctype/orders_item/orders_item.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2024, Faris Ansari and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Orders Item", {
-// 	refresh(frm) {
+frappe.ui.form.on("Orders Item", {
+	// refresh(frm) {
 
-// 	},
-// });
+	// },
+    quantity: function (frm, cdt, cdn) {
+        calculate_amount(frm, cdt, cdn);
+    },
+    price: function (frm, cdt, cdn) {
+        calculate_amount(frm, cdt, cdn);
+    }
+});
+
+function calculate_amount(frm, cdt, cdn) {
+    let row = frappe.get_doc(cdt, cdn); 
+    if (row.quantity && row.price) {
+        row.amount = row.quantity * row.price;
+        frm.refresh_field('items');
+    }
+}
