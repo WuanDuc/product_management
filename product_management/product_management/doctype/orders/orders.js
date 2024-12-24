eead78271314a03aad78d1960676bee2949bcf64
WuanDuc <univerbachtuoc@gmail.com>
2024-12-24 18:25:25 +0700

    Add script for caculating order

--- product_management/product_management/doctype/orders/orders.js
+++ product_management/product_management/doctype/orders/orders.js
@@ -6,3 +6,26 @@
 
 // 	},
 // });
+frappe.ui.form.on('Orders', {
+	refresh(frm) {
+		// your code here
+	}
+})
+frappe.ui.form.on('Orders Item', {
+    // Trigger when Quantity is changed in a row
+    quantity: function (frm, cdt, cdn) {
+        update_amount(frm, cdt, cdn);
+    },
+    // Trigger when Price is changed in a row
+    price: function (frm, cdt, cdn) {
+        update_amount(frm, cdt, cdn);
+    }
+});
+
+function update_amount(frm, cdt, cdn) {
+    let row = frappe.get_doc(cdt, cdn);
+    if (row.quantity && row.price) {
+        row.amount = row.quantity * row.price;
+        frm.refresh_field('items');
+    }
+}
